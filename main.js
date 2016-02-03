1129726bd1305fcac3a7abf90a40ed6f9052c75e
Kieran Startup <info@kieranstartup.co.uk>
2016-02-03 22:58:02 +0000

    this works for everything apart from the three images

--- main.js
+++ main.js
@@ -201,16 +201,16 @@ jQuery('.close-image-3').click(function() {
 
 
 
-// jQuery('.close-image').click(function() {
-//     jQuery('.image-container').css("display","none");
-//     jQuery('img').attr('src', '');
-//     jQuery('img').removeClass("portrait");
-//     jQuery('img').removeClass("landscape");
-//     jQuery('.image-container').removeClass('extended');
-//     jQuery('img').removeClass("image-1");
-//     jQuery('img.reference-image.image-2').remove();
-//     jQuery('img.reference-image.image-3').remove();
-//     enableScroll();
-// });
+jQuery('.close-image').click(function() {
+    jQuery('.image-container').css("display","none");
+    jQuery('img').attr('src', '');
+    jQuery('img').removeClass("portrait");
+    jQuery('img').removeClass("landscape");
+    jQuery('.image-container').removeClass('extended');
+    jQuery('img').removeClass("image-1");
+    jQuery('img.reference-image.image-2').remove();
+    jQuery('img.reference-image.image-3').remove();
+    enableScroll();
+});
 
 });
