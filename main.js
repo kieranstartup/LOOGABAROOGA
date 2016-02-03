7eb30414df605cafe44df440d0b2b92f55eed217
Kieran Startup <info@kieranstartup.co.uk>
2016-02-03 18:58:32 +0000

    working with three images next to each other as well

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ jQuery(document).ready(function() {
 
 jQuery(".image-container").css("display","none");
 
-
-
 function resetCSS(){
 jQuery('.image-container').css("width","70%");
 jQuery('.image-container').css("max-width","800");
@@ -14,6 +12,12 @@ function portraitCSS(){
 jQuery('.image-container').css("width","auto");
 jQuery('.image-container').css("max-width","none");
 jQuery('.image-container').css("height","600");
+}
+
+function threeImageCSS(){
+jQuery('.image-container').css("width","90%");
+jQuery('.image-container').css("max-width","1200");
+jQuery('.image-container').css("height","auto");
 }
 
 function addLandscapeClass(){
@@ -41,66 +45,98 @@ addPortraitClass();
 jQuery('.image-container').show();
 });
 
-
+// Test 3 Images
 jQuery('span#image-3').click(function() {
-jQuery('#image-3-container').show();
+jQuery('.image-container').addClass('extended');
+jQuery('img.reference-image').attr('src', 'images/2.jpeg');
+jQuery('img.reference-image').addClass('image-1');
+jQuery('.image-container').append('<img class="reference-image image-2"/>');
+jQuery('img.reference-image.image-2').attr('src', 'images/2.jpeg');
+jQuery('.image-container').append('<img class="reference-image image-3"/>');
+jQuery('img.reference-image.image-3').attr('src', 'images/2.jpeg');
+threeImageCSS();
+addPortraitClass();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-4').click(function() {
-jQuery('#image-4-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-5').click(function() {
-jQuery('#image-5-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-6').click(function() {
-jQuery('#image-6-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-7').click(function() {
-jQuery('#image-7-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-8').click(function() {
-jQuery('#image-8-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-9').click(function() {
-jQuery('#image-9-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-10').click(function() {
-jQuery('#image-10-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-11').click(function() {
-jQuery('#image-11-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-12').click(function() {
-jQuery('#image-12-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-13').click(function() {
-jQuery('#image-13-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-14').click(function() {
-jQuery('#image-14-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-15').click(function() {
-jQuery('#image-15-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-16').click(function() {
-jQuery('#image-16-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-17').click(function() {
-jQuery('#image-17-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-18').click(function() {
-jQuery('#image-18-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-19').click(function() {
-jQuery('#image-19-container').show();
+jQuery('.image-container').show();
 });
+
 jQuery('span#image-20').click(function() {
-jQuery('#image-20-container').show();
+jQuery('.image-container').show();
 });
+
 
 jQuery('.close-image').click(function() {
     jQuery('.image-container').css("display","none");
+    jQuery('img').attr('src', '');
     jQuery('img').removeClass("portrait");
     jQuery('img').removeClass("landscape");
+    jQuery('img').removeClass("extended");
+    jQuery('img').removeClass("image-1");
+    jQuery('img.reference-image.image-2').remove();
+    jQuery('img.reference-image.image-3').remove();
 });
 
 });
