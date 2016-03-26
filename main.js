f8601ba176bf0f219e36e2de7da0bf8540af36c7
Kieran Startup <info@kieranstartup.co.uk>
2016-03-26 10:25:23 +0000

    updates to the clickable image link images

--- main.js
+++ main.js
@@ -60,9 +60,9 @@ jQuery(document).ready(function() {
         jQuery('.image-container').css("height", "640");
     }
     if (jQuery(window).width() <= 655) {
-        jQuery('.image-container').css("width", "100%");
+        jQuery('.image-container').css("width", "50%");
         jQuery('.image-container').css("height", "auto");
-    }    
+    }   
         disableScroll();
     }
 
