9a56e99cd3aa49d2879c58495fd92265801decbf
Kieran Startup <info@kieranstartup.co.uk>
2016-03-27 12:20:42 +0100

    rewritten the entire image loading code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+jQuery(window).load(function() {
 
     // This all works below
 
@@ -103,20 +103,151 @@ jQuery(document).ready(function() {
     // if (jQuery(window).width() > 414) {
 
         // Portrait Love Loughborough Image
-        jQuery('span#reference-image-1').click(function() {
-            portraitCSS();
-            jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
-            addPortraitClass();
-            jQuery('.image-container').show();
-        });
+        // jQuery('span#reference-image-1').click(function() {
+        //     portraitCSS();
+        //     jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+        //     addPortraitClass();
+        //     jQuery('.image-container').show();
+        // });
 
-        // Landscape Belco Image
-        jQuery('span#reference-image-2').click(function() {
-            resetCSS();
-            jQuery('img.reference-image').attr('src', 'images/reference-image-2-e.jpg');
-            addLandscapeClass();
-            jQuery('.image-container').show();
-        });
+        // // Landscape Belco Image
+        // jQuery('span#reference-image-2').click(function() {
+        //     resetCSS();
+        //     jQuery('img.reference-image').attr('src', 'images/reference-image-2-e.jpg');
+        //     addLandscapeClass();
+        //     jQuery('.image-container').show();
+        // });
+
+
+
+//
+jQuery('span#reference-image-market').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-love-loughborough').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/2.Love_Loughborough.JPG');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-place-branding').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/3.Love_Branding.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-this-bag').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/4.I_Like_My_Town.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-falcon-works').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/5.Falcom_Works.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-immigration').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-postcard').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/7.Postcard.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Portrait
+jQuery('span#reference-image-coat-of-arms').click(function() {
+    portraitCSS();
+    jQuery('img.reference-image').attr('src', 'images/8.Coat_of_Arms.jpg');
+    addPortraitClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-shown-here').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/9.Belco.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-triangle').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-loughborough-town-of-sanctuary').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/11.Town_of_Sanctuary.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-exchange').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/12.John_Storer_House.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-tatting-shuttle').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/13.Tatting Shuttle.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-sports-technology-institute').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Portrait
+jQuery('span#reference-image-tennis-racket-grip').click(function() {
+    portraitCSS();
+    jQuery('img.reference-image').attr('src', 'images/15.Racket_Grip.jpg');
+    addPortraitClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-dismembered-match-balls').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
 
         // 3 Portrait Images
         jQuery('span#reference-image-3').click(function() {
@@ -429,3 +560,10 @@ if (jQuery(window).width() < 414) {
     };
     jQuery(document).ready(main);
 }
+
+
+// jQuery(document).ready(function() {
+
+
+
+// });
---
+++ reference-image-click.js
@@ -0,0 +1,127 @@
+//
+jQuery('span#reference-image-market').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-love-loughborough').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/2.Love_Loughborough.JPG');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-place-branding').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/3.Love_Branding.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-this-bag').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/4.I_Like_My_Town.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-falcon-works').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/5.Falcom_Works.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-immigration').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-postcard').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/7.Postcard.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Portrait
+jQuery('span#reference-image-coat-of-arms').click(function() {
+    portraitCSS();
+    jQuery('img.reference-image').attr('src', 'images/8.Coat_of_Arms.jpg');
+    addPortraitClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-shown-here').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/9.Belco.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-triangle').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-loughborough-town-of-sanctuary').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/11.Town_of_Sanctuary.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-exchange').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/12.John_Storer_House.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+// Landscape
+jQuery('span#reference-image-tatting-shuttle').click(function() {
+    resetCSS();
+    jQuery('img.reference-image').attr('src', 'images/13.Tatting Shuttle.jpg');
+    addLandscapeClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-sports-technology-institute').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
+
+// Portrait
+jQuery('span#reference-image-tennis-racket-grip').click(function() {
+    portraitCSS();
+    jQuery('img.reference-image').attr('src', 'images/15.Racket_Grip.jpg');
+    addPortraitClass();
+    jQuery('.image-container').show();
+});
+
+//
+jQuery('span#reference-image-dismembered-match-balls').click(function() {
+
+    jQuery('img.reference-image').attr('src', 'images/reference-image-1-e.jpg');
+
+    jQuery('.image-container').show();
+});
